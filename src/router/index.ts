c3d6e366fdd0ac371f7515ecf58becce1f9b1ae0
Smirnov Danila <danilasmirnov2077@gmail.com>
2024-09-25 17:45:32 +0500

    1.1

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,17 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
-    path: '/todo-list/',
+    path: 'todo-list/',
     name: 'all',
     component: () => import('../views/AllView.vue'),
   },
   {
-    path: '/todo-list/inWork',
+    path: 'todo-list/inWork',
     name: 'inWork',
     component: () => import('../views/InWorkView.vue'),
   },
   {
-    path: '/todo-list/completed',
+    path: 'todo-list/completed',
     name: 'completed',
     component: () => import('../views/CompletedView.vue'),
   },
